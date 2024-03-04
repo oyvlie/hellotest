fd3ec1011fe002f901add7636c62c8c68abdb89b
oyvlie <oyvind.lie@gmail.com>
2024-03-04 20:17:29 +0100

    01-login-main

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router = express.Router();
+var router = require('express').Router();
+const { requiresAuth } = require('express-openid-connect');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json("Hello World!");
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Auth0 Webapp sample Nodejs',
+    isAuthenticated: req.oidc.isAuthenticated()
+  });
+});
+
+router.get('/profile', requiresAuth(), function (req, res, next) {
+  res.render('profile', {
+    userProfile: JSON.stringify(req.oidc.user, null, 2),
+    title: 'Profile page'
+  });
 });
 
 module.exports = router;
